82b7f30d2dfbd5cfa4aafe73e6783b0b3aee7106
Башара Владислав Олегович <vobashara@edu.hse.ru>
2025-04-27 11:08:51 +0300

    implement logging for genetic_algo

--- src/ldpc-optimizer/include/genetic_algo.hpp
+++ src/ldpc-optimizer/include/genetic_algo.hpp
@@ -12,12 +12,10 @@ struct GeneticMatrix {
     std::string matrix_id;
 };
 
-template<typename CSV>
-void generateLogs(LogSender &sender, CSV& csv, std::string severity, std::string source, std::string messageType, std::string message);
-std::multimap<Result, GeneticMatrix, std::greater<Result>> calculate_obj_func(
-    const BG_type bg_type, const std::multimap<Result, GeneticMatrix, std::greater<Result>> &population_map, const std::pair<double, double> QBER_range,
-    const size_t Z
-    );
-template<typename CSV>
+void generateLogs_console(LogSender &sender, std::string severity, std::string source, std::string messageType, std::string message);
+
+// template<typename T>
+void generateLogs_csv(CSVReceiverT<std::string, std::string, std::string, std::string, std::string> &csv, std::string severity, std::string source, std::string messageType, std::string message);
+
 std::multimap<size_t, std::multimap<IntersectionMetric, GeneticMatrix, std::greater<IntersectionMetric>>> genetic_algo(const size_t popul_size, const double P_m, const std::string mat_path, const size_t Z,
-    const std::pair<double, double> QBER_range, const size_t mu, const size_t iter_amount, const LogSender &logger, const CSV& csv);
+    const std::pair<double, double> QBER_range, const size_t mu, const size_t iter_amount, LogSender &logger, CSVReceiverT<std::string, std::string, std::string, std::string, std::string> &csv);
